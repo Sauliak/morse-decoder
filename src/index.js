cda5e3fb280496c55019b6cbdb28b23acbc534da
Valentin Saulyak <vsaulyak66@gmail.com>
2022-08-09 16:39:59 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const strNumber = expr.match(/.{1,10}/g);
+    let strSymbol = [];
+    let text = '';
+
+    for(let i = 0; i < strNumber.length; i++) {
+        if(strNumber[i] === '**********') {
+            strSymbol.push(' ');
+        } else {
+            const strTwoNum = strNumber[i].match(/.{1,2}/g);
+            let Symbol = '';
+            for (let i = 0; i < strTwoNum.length; i++) {
+                if (strTwoNum[i] === '10') {
+                    Symbol = Symbol + '.';
+                } else if (strTwoNum[i] === '11') {
+                    Symbol = Symbol + '-';
+                }
+            }
+            strSymbol.push(Symbol);
+        }
+    }
+
+    for(let i = 0; i < strNumber.length; i++) {
+        if(strSymbol[i] === ' ' ) {
+            text = text + ' ';
+        } else {
+            text = text + (MORSE_TABLE[strSymbol[i]]);
+        }
+    }
+    return text;
 }
 
 module.exports = {
